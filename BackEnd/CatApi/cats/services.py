7a79955f89ca0d6eceb68b1dfaac4070e0c21116
Juan Solórzano <93926426+juaniitos@users.noreply.github.com>
2023-06-06 15:56:24 -0500

    Actualización: Borrado de codigo innecesario

--- BackEnd/CatApi/cats/services.py
+++ BackEnd/CatApi/cats/services.py
@@ -1,10 +1,10 @@
-import requests
+#import requests
 
-def get_images(requests):
-    url = 'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_yajT9ALrHEvFPItffUkUcnZG1VIBnIRN9vtSu7jJSmILcTqqFmlfBqBkuvL0MWQ9'
-    r = requests.get(url, headers={'Authorization':'Bearer %s' % 'live_yajT9ALrHEvFPItffUkUcnZG1VIBnIRN9vtSu7jJSmILcTqqFmlfBqBkuvL0MWQ9 '})
-    images = r.json()
-    image_list = []
-    for i in range(len(images['images'])):
-        image_list.append(images['images'][i])
-    return image_list
+# def get_images(requests):
+#    url = 'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_yajT9ALrHEvFPItffUkUcnZG1VIBnIRN9vtSu7jJSmILcTqqFmlfBqBkuvL0MWQ9'
+#    r = requests.get(url, headers={'Authorization':'Bearer %s' % 'live_yajT9ALrHEvFPItffUkUcnZG1VIBnIRN9vtSu7jJSmILcTqqFmlfBqBkuvL0MWQ9 '})
+#    images = r.json()
+#    image_list = []
+#    for i in range(len(images['images'])):
+#        image_list.append(images['images'][i])
+#    return image_list
--- BackEnd/CatApi/cats/views.py
+++ BackEnd/CatApi/cats/views.py
@@ -4,17 +4,18 @@ from django.views.generic import TemplateView
 from rest_framework import viewsets
 from .serializer import CatsSerializer
 from .models import Cats
-from .services import get_images
+# from .services import get_images
 
 # Create your views here.
 class CatsView(viewsets.ModelViewSet):
     serializer_class = CatsSerializer
     queryset = Cats.objects.all()
 
-class GetImages(TemplateView):
-    template_name = 'get_images.html'
-    def get_context_data(self, *args, **kwargs):
-        context = {
-            'images' : get_images(),
-        }
-        return context
+# class GetImages(TemplateView):
+#    template_name = 'get_images.html'
+#    template_name = 'get_images.html'
+#    def get_context_data(self, *args, **kwargs):
+#       context = {
+#            'images' : get_images(),
+#        }
+#        return context
